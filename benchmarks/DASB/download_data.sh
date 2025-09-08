5f519773010f5946d3e49fab44eeeadb39cf38ec
mubtasimahasan <65388029+mubtasimahasan@users.noreply.github.com>
2025-09-08 20:25:27 +0000

    fix paths and wandb support

--- benchmarks/DASB/download_data.sh
+++ benchmarks/DASB/download_data.sh
@@ -5,50 +5,58 @@
 #   Target directory: /srv/scratch/tmp/datasets
 # ============================================================
 
-DATASET_DIR=/srv/scratch/tmp/datasets
+DATASET_DIR=/srv/tmp/datasets
 mkdir -p "$DATASET_DIR"
 cd "$DATASET_DIR"
 
-# # -------------------------- LibriSpeech --------------------------
-# echo "Downloading LibriSpeech subset..."
+# -------------------------- LibriSpeech --------------------------
+echo "Downloading LibriSpeech subset..."
 
-# urls=(
-#   "https://openslr.trmal.net/resources/12/train-clean-100.tar.gz"
-#   "https://openslr.trmal.net/resources/12/dev-clean.tar.gz"
-#   "https://openslr.trmal.net/resources/12/test-clean.tar.gz"
-#   "https://openslr.trmal.net/resources/12/test-other.tar.gz"
-# )
+urls=(
+  "https://openslr.trmal.net/resources/12/train-clean-100.tar.gz"
+  "https://openslr.trmal.net/resources/12/dev-clean.tar.gz"
+  "https://openslr.trmal.net/resources/12/test-clean.tar.gz"
+  "https://openslr.trmal.net/resources/12/test-other.tar.gz"
+)
 
-# for url in "${urls[@]}"; do
-#   file=$(basename "$url")
-#   echo "Downloading: $file"
-#   aria2c -x 16 -s 16 "$url"
-#   echo "Extracting: $file"
-#   tar -xvzf "$file"
-#   rm "$file"
-# done
+for url in "${urls[@]}"; do
+  file=$(basename "$url")
+  echo "Downloading: $file"
+  aria2c -x 16 -s 16 "$url"
+  echo "Extracting: $file"
+  tar -xvzf "$file"
+  rm "$file"
+done
 
 
-# -------------------------- SLURP --------------------------
-# echo "Downloading SLURP dataset..."
-# mkdir -p SLURP && cd SLURP
-# aria2c -x 16 -s 16 https://zenodo.org/record/4274930/files/slurp_real.tar.gz
-# aria2c -x 16 -s 16 https://zenodo.org/record/4274930/files/slurp_synth.tar.gz
-# tar -xvzf slurp_real.tar.gz
-# tar -xvzf slurp_synth.tar.gz
-# rm slurp_real.tar.gz slurp_synth.tar.gz
-# aria2c -x 16 -s 16 https://raw.githubusercontent.com/pswietojanski/slurp/master/dataset/slurp/devel.jsonl
-# aria2c -x 16 -s 16 https://raw.githubusercontent.com/pswietojanski/slurp/master/dataset/slurp/test.jsonl
-# aria2c -x 16 -s 16 https://raw.githubusercontent.com/pswietojanski/slurp/master/dataset/slurp/train.jsonl
-# cd ..
+-------------------------- SLURP --------------------------
+echo "Downloading SLURP dataset..."
+mkdir -p SLURP && cd SLURP
+aria2c -x 16 -s 16 https://zenodo.org/record/4274930/files/slurp_real.tar.gz
+aria2c -x 16 -s 16 https://zenodo.org/record/4274930/files/slurp_synth.tar.gz
+tar -xvzf slurp_real.tar.gz
+tar -xvzf slurp_synth.tar.gz
+rm slurp_real.tar.gz slurp_synth.tar.gz
+aria2c -x 16 -s 16 https://raw.githubusercontent.com/pswietojanski/slurp/master/dataset/slurp/devel.jsonl
+aria2c -x 16 -s 16 https://raw.githubusercontent.com/pswietojanski/slurp/master/dataset/slurp/test.jsonl
+aria2c -x 16 -s 16 https://raw.githubusercontent.com/pswietojanski/slurp/master/dataset/slurp/train.jsonl
+cd ..
 
-# # -------------------------- Speech Commands --------------------------
-# echo "Downloading Speech Commands..."
-# mkdir -p SpeechCommands && cd SpeechCommands
-# aria2c -x 16 -s 16 http://download.tensorflow.org/data/speech_commands_v0.02.tar.gz
-# tar -xvzf speech_commands_v0.02.tar.gz
-# rm speech_commands_v0.02.tar.gz
-# cd ..
+# -------------------------- Speech Commands --------------------------
+echo "Downloading Speech Commands..."
+mkdir -p SpeechCommands && cd SpeechCommands
+aria2c -x 16 -s 16 http://download.tensorflow.org/data/speech_commands_v0.02.tar.gz
+tar -xvzf speech_commands_v0.02.tar.gz
+rm speech_commands_v0.02.tar.gz
+cd ..
+
+# # -------------------------- LJSpeech --------------------------
+echo "Downloading LJSpeech..."
+mkdir -p LJSpeech && cd LJSpeech
+aria2c -x 16 -s 16 https://data.keithito.com/data/speech/LJSpeech-1.1.tar.bz2
+tar -xvjf LJSpeech-1.1.tar.bz2
+rm LJSpeech-1.1.tar.bz2
+cd ..
 
 # # -------------------------- Libri2Mix --------------------------
 echo "Cloning LibriMix (for Libri2Mix)..."
@@ -56,14 +64,6 @@ git clone https://github.com/JorisCos/LibriMix
 cd LibriMix
 bash generate_librimix.sh storage_dir
 cd ..
-
-# # -------------------------- LJSpeech --------------------------
-# echo "Downloading LJSpeech..."
-# mkdir -p LJSpeech && cd LJSpeech
-# aria2c -x 16 -s 16 https://data.keithito.com/data/speech/LJSpeech-1.1.tar.bz2
-# tar -xvjf LJSpeech-1.1.tar.bz2
-# rm LJSpeech-1.1.tar.bz2
-# cd ..
 
 # -------------------------- Common Voice Corpus --------------------------
 # echo "Download Common Voice Corpus manually:"
--- benchmarks/DASB/run_test.sh
+++ benchmarks/DASB/run_test.sh
@@ -2,30 +2,30 @@
 
 export CODEC_PATH=/srv/scratch/mub/speech-rep-fusion
 export CHECKPOINT_PATH=/srv/scratch/mub/speech-rep-fusion/saved_files/fusecodec_fusion
+export SAVED_DIR=/srv/tmp
 
-export SAVED_DIR=/srv/scratch/tmp
-mkdir -p $SCRATCH_DIR/data
-mkdir -p $SCRATCH_DIR/cache
-mkdir -p $SCRATCH_DIR/results
+export WANDB_API_KEY=
 
+## run extraction
+# python LibriSpeech/extraction/extract.py \
+#     LibriSpeech/extraction/hparams/speech_tokenizer.yaml \
+#     --data_folder=$SAVED_DIR/datasets/LibriSpeech \
+#     --num_codebooks=8
+
+
+## LibriSpeech
+python LibriSpeech/ASR/train.py \
+    LibriSpeech/ASR/hparams/LSTM/train.yaml \
+    --data_folder $SAVED_DIR/datasets/LibriSpeech \
+    --cached_data_folder $SAVED_DIR/output/cache \
+    --output_folder $SAVED_DIR/output/results/speech_tokenizer \
+    --tokens_folder $SAVED_DIR/output/save/librispeech/ \
+    --seed 1986
+
+
+## Slurp
 # python SLURP/intent_classification/LSTM_linear/train_speech_tokenizer.py \
 #     SLURP/intent_classification/LSTM_linear/hparams/train_speech_tokenizer.yaml \
 #     --output_folder /teamspace/studios/this_studio/output \
 #     --data_folder /teamspace/studios/this_studio/datasets/slurp
 
-
-## run extraction
-python LibriSpeech/extraction/extract.py \
-    LibriSpeech/extraction/hparams/speech_tokenizer.yaml \
-    --data_folder=$SAVED_DIR/datasets/LibriSpeech \
-    --num_codebooks=8
-
-## LibriSpeech
-bash run_experiments.sh \
-    --hparams LibriSpeech/ASR/hparams/LSTM/train.yaml \
-    --data_folder $SAVED_DIR/datasets/LibriSpeech \
-    --cached_data_folder $SAVED_DIR/cache/ \
-    --output_folder $SAVED_DIR/outputs/results/speech_tokenizer/save/librispeech \
-    --task ASR --dataset LibriSpeech \
-    --seed 1986 --nruns 2 --eval_metric WER \
-    --tokens_folder $SAVED_DIR/data/LibriSpeech/extraction-emb/speech_tokenizer/save/librispeech/
